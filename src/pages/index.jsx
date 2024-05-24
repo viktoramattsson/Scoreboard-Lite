a1b3626be470d016d351221bfe0e84cfcea9b846
Viktor Mattsson <viktor.a.mattsson@gmail.com>
2024-05-24 14:58:12 +0200

    add load a saved game

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import db from '../db';
+import { FaTimes } from 'react-icons/fa';
+import { useRouter } from 'next/router'; // Importing useRouter from next/router
 
 const Home = () => {
   const [savedGames, setSavedGames] = useState([]);
   const [showSavedGames, setShowSavedGames] = useState(false);
+  const router = useRouter(); // Initializing router
 
   useEffect(() => {
     const fetchSavedGames = async () => {
@@ -21,6 +24,14 @@ const Home = () => {
       fetchSavedGames();
     }
   }, [showSavedGames]);
+
+  const handleLoadGame = (game) => {
+    localStorage.setItem('playerNames', JSON.stringify(game.playerNames));
+    localStorage.setItem('scores', JSON.stringify(game.scores));
+    localStorage.setItem('currentRound', game.currentRound);
+    localStorage.setItem('gameMode', game.gameMode);
+    router.push('/playRounds');
+  };
 
   return (
     <>
@@ -43,15 +54,26 @@ const Home = () => {
           Saved Games
         </button>
         {showSavedGames && (
-          <div className="mt-4 p-4 bg-gray-100 rounded-lg">
-            <h2 className="text-lg font-bold mb-2">Saved Games</h2>
-            <ul>
-              {savedGames.map((game) => (
-                <li key={game.id} className="mb-1">
-                  {game.title} spel
-                </li>
-              ))}
-            </ul>
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-60">
+            <div className="bg-blue-300 p-6 rounded-lg shadow-lg relative w-4/5 max-w-80 min-h-[50%]">
+              <h2 className="text-lg font-bold mb-2">Saved Games</h2>
+              <ul>
+                {savedGames.map((game) => (
+                  <li key={game.id} className="mb-1">
+                    <div
+                      className="bg-green-200 rounded-lg h-10 text-center flex items-center justify-center cursor-pointer"
+                      onClick={() => handleLoadGame(game)}
+                    >
+                      {game.title}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+              <FaTimes
+                className="absolute top-2 right-2 text-3xl cursor-pointer text-black"
+                onClick={() => setShowSavedGames(false)}
+              />
+            </div>
           </div>
         )}
       </div>
